d5d6ba6caf59e1447c98938a61b261a7c1adfa83
SmitaAmrutkar <smitaamrutkar55@gmail.com>
2025-05-03 10:30:44 +0530

    Initial commit: Auth API project

---
+++ routes/authRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+router.post('/register', authController.register);
+router.post('/login', authController.login);
+
+// Example of a protected route
+router.get('/profile', authMiddleware, (req, res) => {
+  res.json({ message: 'This is a protected profile route', userId: req.user.id });
+});
+
+module.exports = router;
